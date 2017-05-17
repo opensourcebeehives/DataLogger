d5ef4be708aed0647db216e7160995662debb635
tlowe2 <ted.lowe.2@gmail.com>
2017-05-17 09:38:06 -0600

    Add battery check, main loop as function

--- SleepyPi_FW/SleepyPi_FW.ino
+++ SleepyPi_FW/SleepyPi_FW.ino
@@ -24,6 +24,8 @@ bool PI_WAKING_UP_FROM_SLEEP = true;
 // Counter for number of times to check before switching off
 int deadCounter = 0;
 int videoCounter = 0;
+// Battery Voltage Dead Value, where 1 = 0.06V, in this case 156 = 9.4V
+int deadValue = 156;
 
 // Globals
 // ++++++++++++++++++++ Change me ++++++++++++++++++
@@ -45,6 +47,9 @@ void setup()
   pinMode(LED_PIN, OUTPUT);		
   digitalWrite(LED_PIN,HIGH);		// Switch on LED
 
+  // Battery Voltage ADC
+  pinMode(A6, INPUT);
+
   SleepyPi.enablePiPower(false);
   SleepyPi.enableExtPower(false);
 
@@ -59,6 +64,16 @@ void setup()
   delay(200);
   digitalWrite(LED_PIN,LOW);   // Switch off LED
 
+  Serial.println("Initial piHandler");
+  piHandler();
+  
+/*
+  // Check the battery voltage, a value of 1 = 0.06V, if it is too low, go back to sleep
+  if (analogRead(A6) <= deadValue){
+    flashLED(6, 500);
+    break;
+  }
+    
   // It will sit in this loop until the dead counter gets too high
   while(1)
   {
@@ -89,6 +104,7 @@ void setup()
     //}
     delay(5000);
   }
+  */
 }
 
 void loop() 
@@ -100,6 +116,8 @@ void loop()
 
     // Allow wake up alarm to trigger interrupt on falling edge.
     attachInterrupt(0, alarm_isr, FALLING);    // Alarm pin
+
+    Serial.println("Sleeping...");
 
     // Set the Periodic Timer
     SleepyPi.setTimer1(PeriodicTimer_Timebase, PeriodicTimer_Value);
@@ -115,48 +133,16 @@ void loop()
     
     SleepyPi.ackTimer1();
 
-    // It will sit in this loop until the dead counter gets too high
-    while(1)
-    {
-      //
-      //  TO ADD: check voltage of batteries on pin A6, if too low, flash LED and switch back off
-      //
-      
-      // If just finished the interrupt, wake up the pi
-      if (PI_WAKING_UP_FROM_SLEEP){
-        wakePiUp();
-      }else{
-        if(SleepyPi.checkPiStatus(false)){  // If the pi is awake, reset the dead counter
-          deadCounter = 0;
-          videoCounter++;
-          //digitalWrite(LED_PIN, HIGH);
-          //Serial.println("Pi awake");
-          Serial.println(videoCounter);
-          
-        }else{                              // If the pi is not detected, count up on the dead counter
-          digitalWrite(LED_PIN, LOW);
-          videoCounter = 0;
-          deadCounter++;
-          Serial.println(deadCounter);
-        }
-      }
-      if (deadCounter >= 6){ // If the pi still isn't detected, it hasn't switched on in time or is totally shut down, so kill it
-        deadCounter = 0;
-        break;
-      }
-      //if (videoCounter == 4){
-      //  SleepyPi.enableExtPower(false);
-      //}
-      delay(5000);
-    }
+    Serial.println("Main piHandler");
+    piHandler();
 
-    digitalWrite(LED_PIN,HIGH);    // Switch on LED
-    delay(50);
-    digitalWrite(LED_PIN,LOW);    // Switch off LED 
-    delay(50);
-    digitalWrite(LED_PIN,HIGH);    // Switch on LED
-    delay(50);
-    digitalWrite(LED_PIN,LOW);    // Switch off LED   
+    //digitalWrite(LED_PIN,HIGH);    // Switch on LED
+    //delay(50);
+    //digitalWrite(LED_PIN,LOW);    // Switch off LED 
+    //delay(50);
+    //digitalWrite(LED_PIN,HIGH);    // Switch on LED
+    //delay(50);
+    //digitalWrite(LED_PIN,LOW);    // Switch off LED   
 }
 
 void wakePiUp() {
@@ -168,5 +154,61 @@ void wakePiUp() {
   //SleepyPi.enableExtPower(true);
   digitalWrite(LED_PIN, HIGH);
   PI_WAKING_UP_FROM_SLEEP = false;
+}
+
+void flashLED(int amount, int delaytime){
+  int ii;
+
+  for (ii = 0; ii < amount; ii++){
+    digitalWrite(LED_PIN,HIGH);    // Switch on LED
+    delay(delaytime);
+    digitalWrite(LED_PIN,LOW);    // Switch off LED 
+    delay(delaytime);
+  }
+}
+
+void piHandler(){
+  // Check the battery voltage, if it is too low, go back to sleep
+  Serial.println("Battery check");
+  if (analogRead(A6) <= deadValue){
+    Serial.println("Batteries low, exiting");
+    flashLED(6, 500);
+    return;
+  }
+  // It will sit in this loop until the dead counter gets too high
+  Serial.println("Main loop");
+  while(1)
+  {      
+    // If just finished the interrupt, wake up the pi
+    if (PI_WAKING_UP_FROM_SLEEP){
+      Serial.println("Waking up pi");
+      wakePiUp();
+    }else{
+      if(SleepyPi.checkPiStatus(false)){  // If the pi is awake, reset the dead counter
+        deadCounter = 0;
+        videoCounter++;
+        //digitalWrite(LED_PIN, HIGH);
+        Serial.println("Pi awake");
+        //Serial.println(videoCounter);
+        
+      }else{                              // If the pi is not detected, count up on the dead counter
+        digitalWrite(LED_PIN, LOW);
+        //videoCounter = 0;
+        deadCounter++;
+        Serial.print("Dead counter: ");
+        Serial.println(deadCounter);
+      }
+    }
+    if (deadCounter >= 6){ // If the pi still isn't detected, it hasn't switched on in time or is totally shut down, so kill it
+      Serial.println("Pi not detected, exiting");
+      flashLED(2, 50);
+      deadCounter = 0;
+      break;
+    }
+    //if (videoCounter == 4){
+    //  SleepyPi.enableExtPower(false);
+    //}
+    delay(5000);
+  }
 }
 
